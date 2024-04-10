fcb5c84d7473ddc1ebf77ee28afbcc4307989dfe
jatintechies11 <jatintechies11@gmail.com>
2024-04-10 11:45:43 +0530

    Modules file add

---
+++ Modules.js
@@ -0,0 +1,16 @@
+// React ES6 modules refer to the way you can organize and import/export code in React applications using ES6 module syntax. ES6 modules provide a more structured and efficient way to manage dependencies and share code between different parts of your application.
+
+// Import named exports from the file person.js:
+// import { name, age } from "./person.js";
+
+// Import a default export from the file message.js:
+// import message from "./message.js";
+
+// You can only have one default export in a file.
+// const message = () => {
+//     const name = "Jesse";
+//     const age = 40;
+//     return name + ' is ' + age + 'years old.';
+//   };
+  
+//   export default message;
