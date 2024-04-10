17f5eb3b7d212693b63ae90246ffea1f7754b4ea
jatintechies11 <jatintechies11@gmail.com>
2024-04-10 12:53:31 +0530

    Class file

--- Classes.js
+++ Classes.js
@@ -4,11 +4,13 @@
 
 // Class Declaration: Aap class keyword ke sath class ka naam likh kar class declare kar sakte hain.
 
+// Constructor Method: constructor method ek special method hai jo class se banaye gaye objects ko create aur initialize karne ke liye hota hai. Ye khud ba khud execute hota hai jab class ka ek naya instance banaya jata hai.
+
 // class MeraClass {
 //     // class methods aur constructor
 // }
 
-// Constructor Method: constructor method ek special method hai jo class se banaye gaye objects ko create aur initialize karne ke liye hota hai. Ye khud ba khud execute hota hai jab class ka ek naya instance banaya jata hai.
+// Class Methods: Aap class ke andar methods ko define kar sakte hain, jaise ki functions. Ye methods class ke instances par call kiye ja sakte hain.
 
 // class MeraClass {
 //     constructor(prop1, prop2) {
@@ -17,8 +19,7 @@
 //     }
 // }
 
-// Class Methods: Aap class ke andar methods ko define kar sakte hain, jaise ki functions. Ye methods class ke instances par call kiye ja sakte hain.
-
+// Inheritance: Classes extends keyword ke zariye inheritance ko support karte hain. Yeh ek subclass (child class) ko ek superclass (parent class) se inherit karne ki anumati deta hai.
 
 // class MeraClass {
 //     constructor(prop1, prop2) {
@@ -31,7 +32,7 @@
 //     }
 // }
 
-// Inheritance: Classes extends keyword ke zariye inheritance ko support karte hain. Yeh ek subclass (child class) ko ek superclass (parent class) se inherit karne ki anumati deta hai.
+// ES6 mein classes JavaScript mein object-oriented programming ke concepts ko define karne ke liye ek zyada structured aur intuitive tareeka pradan karte hain. Ye syntax ko simplify karte hain aur complex code structures ke saath kaam karne ko aasan banate hain.
 
 // class MeraSubClass extends MeraClass {
 //     constructor(prop1, prop2, prop3) {
@@ -42,14 +43,12 @@
 //     // subclass ke liye khas methods
 // }
 
-// Mukhyatah, ES6 mein classes JavaScript mein object-oriented programming ke concepts ko define karne ke liye ek zyada structured aur intuitive tareeka pradan karte hain. Ye syntax ko simplify karte hain aur complex code structures ke saath kaam karne ko aasan banate hain.
 
 // The super() method refers to the parent class.
 // By calling the super() method in the constructor method, we call the parent's constructor method and get access to the parent's properties and methods.
 
 // To create a class inheritance, use the extends keyword.
 // A class created with a class inheritance inherits all the methods from another class:
-
 
 // Class declaration
 class Animal {
